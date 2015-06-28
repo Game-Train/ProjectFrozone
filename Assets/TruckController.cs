22edbab42f7b67f080d2d7b21bfb6f4a68021e2d
bonbombs <kelly.zhang2306@gmail.com>
2015-06-28 17:35:00 -0400

    Added Unity assets folder and files
    Added Assets folder. Contains:
    - Main scene w/ cube object and mock UI
    - TruckController.cs
    
    Currently can:
    - Move truck/cube around

---
+++ Assets/TruckController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TruckController : MonoBehaviour {
+
+	public float speed = 10;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		Vector2 direction = new Vector2 (x, y);
+
+		rb.velocity = speed * direction;
+
+	}
+}
